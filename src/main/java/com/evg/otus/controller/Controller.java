6c804fe20cabec70f8170b3f183eb1bf1c7ce28c
evgeniya <ushakovazenya@gmail.com>
2023-07-03 21:14:47 +0300

    OTUS-1 start build with docker

---
+++ src/main/java/com/evg/otus/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.evg.otus.controller;
+
+import com.evg.otus.dto.HealthResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/health/")
+    public HealthResponse getHealth() {
+        return new HealthResponse("OK");
+    }
+    
+}
