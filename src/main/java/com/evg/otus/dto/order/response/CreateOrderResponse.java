65555aa6bf3243b3f1546f5247b98fbf0da11c9f
evgeniya <ushakovazenya@gmail.com>
2023-10-24 22:50:39 +0300

    OTUS-6 - service Order idempotencyKey

--- src/main/java/com/evg/otus/dto/order/response/CreateOrderResponse.java
+++ src/main/java/com/evg/otus/dto/order/response/CreateOrderResponse.java
@@ -2,6 +2,7 @@ package com.evg.otus.dto.order.response;
 
 import com.evg.otus.dto.BaseResponse;
 import com.evg.otus.dto.order.ProductDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
@@ -15,6 +16,7 @@ public class CreateOrderResponse extends BaseResponse {
 
     private String orderKey;
     private Long orderId;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<ProductDto> products;
 
 }
